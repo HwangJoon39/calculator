e549cd61612d078fe67c0931ec1132b69954fe18
hwangjoon <hjoon39@naver.com>
2026-03-23 11:35:48 +0900

    STEP 1에서 구현한 App 클래스의 main 메서드에 Calculator 클래스가 활용될 수 있도록 수정

---
+++ src/step2/app.java
@@ -0,0 +1,38 @@
+package step2;
+
+import java.util.Scanner;
+
+public class app {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Calculator 객체 생성
+        Calculator cal = new Calculator();
+
+        while (true) {
+            System.out.print("첫 번째 숫자를 입력해주세요: ");
+            int num1 = sc.nextInt();
+
+            System.out.print("두 번째 숫자를 입력해주세요: ");
+            int num2 = sc.nextInt();
+
+            System.out.print("사칙연산 기호를 입력해주세요: ");
+            char operator = sc.next().charAt(0);
+
+            //result 변수에 저장
+            int result = cal.calculate(num1, num2, operator);
+
+            System.out.println("결과는 " + result + " 입니다.");
+
+            System.out.println("계산기를 종료하시겠습니까? (exit 입력 시 종료)");
+            String exit = sc.next();
+            if (exit.equals("exit")) {
+                System.out.println("계산기를 종료합니다.");
+                break;
+            } else {
+                System.out.println("계산기를 계속 실행합니다.");
+            }
+        }
+    }
+}
